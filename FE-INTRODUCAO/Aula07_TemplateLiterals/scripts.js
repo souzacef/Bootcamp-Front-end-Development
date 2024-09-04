cfa6f27764aea958c666aa4a4676a4541e5f2529
Carlos <souzacef@gmail.com>
2024-09-04 16:53:56 -0300

    Aula 07

---
+++ FE-INTRODUCAO/Aula07_TemplateLiterals/scripts.js
@@ -0,0 +1,17 @@
+let nome = 'João';
+let sobrenome = 'Silva'
+// let saudacao = 'Olá! meu nome é' + ' ' + nome
+let saudacao = `Olá! Meu nome é ${nome} ${sobrenome}`;
+console.log(saudacao);
+
+let preco = 10;
+let quantidade = 3;
+let mensagemTotal = `O total foi ${preco * quantidade} reais`;
+
+console.log(mensagemTotal);
+
+const meuTexto = `Olá
+Seja bem vindo
+Até breve`;
+
+console.log(meuTexto);
